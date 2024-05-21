c72e8bb499dd5a377a20f5f64371eed29fa456e2
milenatavares <milenatavares@lampp-it.com.br>
2024-05-21 14:33:54 -0300

    model, migrations, seeds e factories

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Post::factory()
+            ->count(100)
+            ->create();
+    }
+}
