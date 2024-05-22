a91d1d8aa0ff182cd58a53b877b931d3ca9adb7d
milenatavares <milenatavares@lampp-it.com.br>
2024-05-22 11:54:02 -0300

    Finalização do módulo 03: Notifications Email

--- app/Http/Controllers/Posts/CommentController.php
+++ app/Http/Controllers/Posts/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Posts;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCommentFormRequest;
+use App\Notifications\PostComment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -11,6 +12,9 @@ class CommentController extends Controller
     public function store(StoreCommentFormRequest $request)
     {
         $comment = $request->user()->comments()->create($request->all());
+
+        $author = $comment->post->user; // Pega o autor do post
+        $author->notify(new PostComment($comment)); // Envia a notificação de que houve um comentário no post para o author do post
 
         return redirect()->route('posts.show', $comment->post_id)->withSuccess('Comentário realizado com sucesso!');
     }
---
+++ app/Notifications/PostComment.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Comment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PostComment extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    private $comment;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject("Novo comentário de {$this->comment->title}")
+                    ->line($this->comment->body)
+                    ->action('VEja o comentário', route('posts.show', $this->comment->post_id))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
